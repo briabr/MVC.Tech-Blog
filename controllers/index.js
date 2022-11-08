e32148f6191867559df20ef56313f6bec4e09ad7
briabr <filalibria@yahoo.fr>
2022-11-07 22:58:11 -0500

    finished models

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homePageRoutes = require('./homepage-routes.js');
+const dashbboardRoutes = require('./dashboard-routes.js');
+
+
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homePageRoutes);
+
+
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+
+module.exports = router;
